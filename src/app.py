a173aecb38a415fb31ef687064cd5cdb37e18797
Reza Mousavi <Reza.moosavi21@gmail.com>
2024-05-04 13:49:37 +0330

    update the app file

--- src/app.py
+++ src/app.py
@@ -5,17 +5,18 @@ def main():
     st.title("Movie Recommendation App")
 
     # Load movie data
-    movie_data = data_handling.load_data("data/movies.csv")
+    movie_data = data_handling.load_data("src/data/movies.csv")
 
-    # Sidebar for user input
-    user_input = ui_design.get_user_input(movie_data)
+    if movie_data is not None:
+        # Sidebar for user input
+        user_input = ui_design.get_user_input(movie_data)
 
-    if user_input:
-        # Generate movie recommendations
-        recommended_movies = recommendation.get_recommendations(user_input, movie_data)
+        if user_input:
+            # Generate movie recommendations
+            recommended_movies = recommendation.get_recommendations(user_input, movie_data)
 
-        # Display recommendations
-        ui_design.display_recommendations(recommended_movies)
+            # Display recommendations
+            ui_design.display_recommendations(recommended_movies)
 
 if __name__ == "__main__":
     main()
